62dbe3484506340dcd1b0bb9d59af9b308055bad
Carlos <carloscastro96@live.com.pt>
2026-01-26 03:00:37 +0000

    my-first-react-app(Movies).wip

---
+++ react/my-first-react-app/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  return (
+    <div className="card" >
+      <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(true)}>
+        Like
+      </button>
+    </div>
+  )
+}
+
+const App = () => {
+  
+
+
+  return (
+    <div className="card-container" >
+      <Card title="Star Wars" rating={5} isCool={true}/>
+      <Card title="Avatar"/>
+      <Card title="Lion King"/>
+    </div>
+  )
+}
+
+
+export default App
